646d181dd62c07d4c2c246257f4e353654e158af
Omkar Gandhi <gandhioms16@gmail.com>
2024-03-25 12:36:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthProvider from "./hooks/AuthProvider";
+import HomePage from "./pages/Home";
+import PageNotFound from "./pages/NoPage";
+import LoginPage from "./pages/LoginRegister";
+import Dash from "./pages/UserDashboard/Dash";
+import TicketForm from "./components/TicketForm";
+import { UserRoute, TechSupportRoute, AdminRoute } from "./router/PrivateRoute";
+import AdminDash from "./pages/admin/Admin";
+import { AdminTicketProvider } from "./contexts/AdminTicketContext";
+import { TechSupportProvider } from "./contexts/TechSupportContext";
+import TechSupportDash from "./pages/techsupport/TechSupportDashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <AuthProvider>
+          <AdminTicketProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route element={<UserRoute />}>
+                <Route path="/userdash" element={<Dash />} />
+                <Route path="/createticket" element={<TicketForm />} />
+              </Route>
+              <Route element={<TechSupportRoute />}>
+                <Route path="/techsupportdash" element={<TechSupportDash />} />
+              </Route>
+            </Routes>
+            <TechSupportProvider>
+              <Routes>
+                <Route element={<AdminRoute />}>
+                  <Route path="/admin" element={<AdminDash />} />
+                </Route>
+              </Routes>
+            </TechSupportProvider>
+          </AdminTicketProvider>
+        </AuthProvider>
+        <Routes>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
